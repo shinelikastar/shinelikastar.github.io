048f2484197c866b8dff82cef1b90a519666e954
star <star_su@brown.edu>
2020-08-01 14:23:47 -0400

    Updates

---
+++ precache-manifest.b44a7dbe9de832c7df29fb52bfe73ac9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5ac582c8a688879efd8c57e1f1fa00c",
+    "url": "/github.io/index.html"
+  },
+  {
+    "revision": "c9b1b29cd8c5f2810bc6",
+    "url": "/github.io/static/css/main.e6b1c7e9.chunk.css"
+  },
+  {
+    "revision": "acf9c11ca3becfe41889",
+    "url": "/github.io/static/js/2.dc6b3f89.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/github.io/static/js/2.dc6b3f89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9b1b29cd8c5f2810bc6",
+    "url": "/github.io/static/js/main.667ab6d4.chunk.js"
+  },
+  {
+    "revision": "9de90267d0bce1968753",
+    "url": "/github.io/static/js/runtime-main.eca7a870.js"
+  },
+  {
+    "revision": "2a009e4a523e39f42d642e912306c0e3",
+    "url": "/github.io/static/media/diva.2a009e4a.png"
+  },
+  {
+    "revision": "ca5d308d7ae07e612a7f097d8ba9260b",
+    "url": "/github.io/static/media/eggs_static.ca5d308d.png"
+  },
+  {
+    "revision": "b9731e88419169f455b1fafe27bd5470",
+    "url": "/github.io/static/media/maps_static.b9731e88.png"
+  },
+  {
+    "revision": "951c1d7f7b8d0daf3bacc2655a26295f",
+    "url": "/github.io/static/media/pointing-up.951c1d7f.png"
+  },
+  {
+    "revision": "7feb7caa95f1d2560bf120f8f471213b",
+    "url": "/github.io/static/media/raisin_bread_static.7feb7caa.png"
+  },
+  {
+    "revision": "ffc8006678deba068e2c31bed7f248a6",
+    "url": "/github.io/static/media/weenix.ffc80066.gif"
+  }
+]);
