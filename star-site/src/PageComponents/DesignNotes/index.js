48bd835b7b192e6a4dabe424e41e69274173598c
star <star_su@brown.edu>
2020-08-01 15:27:04 -0400

    continue polish on lower part of page

--- star-site/src/PageComponents/DesignNotes/index.js
+++ star-site/src/PageComponents/DesignNotes/index.js
@@ -10,9 +10,10 @@ function DesignNotes() {
         <p>
           This is a space to name all the wondrous things that resulted in the
           creation of this website. If you found any amount of delight in the
-          drawings (of me in various states), these were done by my talented
-          sister Floria. I am grateful for her patience. She has an uncanny
-          ability to listen to my indecision and pick a side.
+          drawings (of me in various states), these were done by my{" "}
+          <a href="https://floriatsui.github.io/">talented sister Floria</a>. I
+          am grateful for her patience. She has an uncanny ability to listen to
+          my indecision and pick a side.
           <br></br>
           <br></br>
           The headers are set in Rowdies, the body in Jost. If you take
