b1a2826446ac11210627299811aca6407e47e03a
star <star_su@brown.edu>
2021-03-26 15:35:23 -0400

    Updates

---
+++ precache-manifest.711fb90ce6a1517033c4f1398843439c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53e21eaa7c2a9abfdb914d8a1aee033a",
+    "url": "/starcsu.com/index.html"
+  },
+  {
+    "revision": "c78d314f31273cef9531",
+    "url": "/starcsu.com/static/css/main.53f4d822.chunk.css"
+  },
+  {
+    "revision": "dce00e03e8515ae1a33a",
+    "url": "/starcsu.com/static/js/2.c5c8f934.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/starcsu.com/static/js/2.c5c8f934.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c78d314f31273cef9531",
+    "url": "/starcsu.com/static/js/main.6ce16d8a.chunk.js"
+  },
+  {
+    "revision": "499dbb0b42f5a87bce37",
+    "url": "/starcsu.com/static/js/runtime-main.9a778d07.js"
+  },
+  {
+    "revision": "2a009e4a523e39f42d642e912306c0e3",
+    "url": "/starcsu.com/static/media/diva.2a009e4a.png"
+  },
+  {
+    "revision": "54f74e8ce6d08d4cba5fea5832f989b1",
+    "url": "/starcsu.com/static/media/dozing.54f74e8c.png"
+  },
+  {
+    "revision": "951c1d7f7b8d0daf3bacc2655a26295f",
+    "url": "/starcsu.com/static/media/pointing-up.951c1d7f.png"
+  }
+]);
