cc0aa229baa90b5773010c34c5befc2c1230efa4
star <star_su@brown.edu>
2021-03-26 15:36:55 -0400

    Updates

---
+++ precache-manifest.6bca59808fbb2fd1b5e617fab347d2a8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a4fac5d51f9c9bca9933c77a319c9f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7aa87ae33fd0c073b7ec",
+    "url": "/static/css/main.53f4d822.chunk.css"
+  },
+  {
+    "revision": "dce00e03e8515ae1a33a",
+    "url": "/static/js/2.c5c8f934.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.c5c8f934.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7aa87ae33fd0c073b7ec",
+    "url": "/static/js/main.cb2d4cfc.chunk.js"
+  },
+  {
+    "revision": "7cd3d7a5d9a32a7aa414",
+    "url": "/static/js/runtime-main.da063aa9.js"
+  },
+  {
+    "revision": "2a009e4a523e39f42d642e912306c0e3",
+    "url": "/static/media/diva.2a009e4a.png"
+  },
+  {
+    "revision": "54f74e8ce6d08d4cba5fea5832f989b1",
+    "url": "/static/media/dozing.54f74e8c.png"
+  },
+  {
+    "revision": "951c1d7f7b8d0daf3bacc2655a26295f",
+    "url": "/static/media/pointing-up.951c1d7f.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/starcsu.com/precache-manifest.711fb90ce6a1517033c4f1398843439c.js"
+  "/precache-manifest.6bca59808fbb2fd1b5e617fab347d2a8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/starcsu.com/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
